255df3292e59ff19d8a064576664be725dac3bab
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-03-01 00:00:02 +0100

    New try for the nav

---
+++ js/nourritoi_nav.js
@@ -0,0 +1,9 @@
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $('#custom-nav').addClass('affix');
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $('#custom-nav').removeClass('affix');
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
